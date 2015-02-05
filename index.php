5f8450188a42ca379dcffb309336a03edcf21965
palmerjoshua <palmerjoshua2013@fau.edu>
2015-02-05 16:42:35 -0500

    added github link to front page

--- index.php
+++ index.php
@@ -17,10 +17,12 @@
 
 <!-- Main content -->
 <div class="jumbotron">            
-    <h1 style="text-align: center;">COP 3813</h1>
+    <h1 style="text-align: center;">COP 3813 </h1>
     <h2>Introduction to Internet Computing</h2>
-    <p>The assignments and projects I completed during the Spring 2015 Semester</p>
+    <p>The assignments and projects I completed during the Spring 2015 Semester <a href="https://github.com/palmerjoshua/COP3813" title="GitHub" target="_blank"><span class="glyphicon glyphicon-link"></span></a></p>
+     
     <p class="text-muted">Background image by <a href="http://redd.it/2umxab" target="_blank">/u/StupidSolipsist</a></p>
+    <p></p>
 </div>
 
 
