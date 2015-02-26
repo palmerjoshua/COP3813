8ea403d7c04f50390b553fb107aa05c51caa49fe
palmerjoshua <palmerjoshua2013@fau.edu>
2015-02-26 18:07:20 -0500

    filters now layer. need to add more, and fix text adding

--- Project_1/imgEditor.js
+++ Project_1/imgEditor.js
@@ -2,13 +2,92 @@ var imagesource = '../img/main_background.jpg',
     canvas = document.getElementById('postcard'),
     context = canvas.getContext('2d');
 
-var original,  // unaltered image
-    before,   // image state before each new filter is applied
-    current; // image state immediately after each new filter is applied
-
 var imageData = function(){return context.getImageData(0, 0, canvas.width, canvas.height);}
 
-var lastfiltername; // name of last filter applied
+var original = null;
+
+var filters = {};
+
+var functions = {
+    
+    
+    'grayscale': function(value){
+        
+        var imgdata = imageData(),
+        data = imgdata.data;
+    
+        for(var i=0; i<data.length; i += 4){
+        var avg = Math.round((data[i]+data[i+1]+data[i+2]) / 3);
+        data[i] = data[i+1] = data[i+2] = avg;
+        }
+        putData(imgdata); 
+    },
+    
+    
+    'sepia': function(value){
+        
+        var imgdata = imageData(),
+        data = imgdata.data;   
+
+        for(var i=0; i<data.length; i += 4){
+        var red = Math.round((data[i] * .393)+(data[i+1] * .769)+(data[i+2]*.189)),
+            green = Math.round((data[i] * .349)+(data[i+1] * .686)+(data[i+2]*.168)),
+            blue = Math.round((data[i] * .272)+(data[i+1] * .534)+(data[i+2]*.131));
+
+            data[i] = (red <= 255) ? red : 255;
+            data[i+1] = (green <= 255) ? green : 255;
+            data[i+2] = (blue <= 255) ? blue : 255;
+        }
+        putData(imgdata);   
+    },
+    
+    
+    'invert': function(value){
+        
+        var imgdata = imageData(),
+            data = imgdata.data;   
+        for(var i=0; i<data.length; i += 4){
+            data[i] = 255 - data[i];
+            data[i+1] = 255 - data[i+1];
+            data[i+2] = 255 - data[i+2];
+        }
+        putData(imgdata);
+    },
+    
+    
+    'brightness': function(value){
+       
+        var imgdata = imageData(),
+            data = imgdata.data;
+
+        for(var i=0; i<data.length; i += 4){
+            data[i] = clamp(data[i]*value);
+            data[i+1] = clamp(data[i+1]*value);
+            data[i+2] = clamp(data[i+2]*value);
+        }
+        putData(imgdata);
+        
+    },
+    
+    
+    'contrast': function(value){
+    
+        var imgdata = imageData(),
+            data = imgdata.data;
+
+        for(var i=0; i<data.length; i += 4){
+            var avg = Math.round((data[i]+data[i+1]+data[i+2]) / 3),
+                red = (avg >= 128) ? data[i]/value : data[i]*value,
+                green = (avg >= 128) ? data[i+1]/value : data[i+1]*value,
+                blue = (avg >= 128) ? data[i+2]/value : data[i+2]*value;
+
+            data[i] = clamp(red);
+            data[i+1] = clamp(green);
+            data[i+2] = clamp(blue);
+        }
+        putData(imgdata);       
+    }
+};
 
 
 function uploadImage(input){
@@ -26,24 +105,12 @@ function uploadImage(input){
 
 
 function newCanvasImage(source){
-    original = before = current = null;
     var image = new Image();
     image.src = source;
     image.onload = function(){
         context.drawImage(image,0,0, image.width, image.height, 0,0, canvas.width, canvas.height);
-        original = before = imageData();
+        original = imageData();
     };
-    //original = image;
-}
-
-
-function revert(){   
-    /*if no before, start from original
-      else if new filter being used, before = current
-      else before stays the same
-    */   
-    putData(before);
-    //context.drawImage(currentimage, 0, 0, currentimage.width, currentimage.height, 0, 0, canvas.width, canvas.height);
 }
 
 
@@ -69,8 +136,41 @@ function clamp(value){
 }
 
 
-function contrast(value){
-
+function update(name, value){
+    switch(name){// button filters don't layer
+        case 'grayscale':
+            if('sepia' in filters){
+                delete filters['sepia'];
+            }
+            if('invert' in filters){
+                delete filters['invert'];
+            }
+            break;
+        case 'sepia':
+            if('grayscale' in filters){
+                delete filters['grayscale'];
+            }
+            if('invert' in filters){
+                delete filters['invert'];
+            }
+            break;
+        case 'invert':
+            if('sepia' in filters){
+                delete filters['sepia'];
+            }
+            if('grayscale' in filters){
+                delete filters['grayscale'];
+            }
+        default:
+            break;
+    }
+    
+    
+    filters[name] = value;
+    putData(original);
+    for(var filter in filters){
+        functions[filter](filters[filter]);
+    }
 }
 
 
@@ -88,121 +188,46 @@ $('#ChoosePicture').change(function(){
 
 $('#reset').click(function(){
     putData(original);
-    before = current = original;
-    lastfiltername = null;
-    
+    filters = {};
     $('p').text('');
 });
 
 
-function beforestate(){
-
-    return (before == original)?'before = original': (before==current)? 'before = current' : 'before=before';
-    
-}
-
 $('#brightness').change(function(){ 
     
-    
-    var currentname = $(this).attr('id');
-    
-    if(currentname != lastfiltername){
-        before = current;
-    }
-    revert();
-    var value = $(this).val(),     
-        imgdata = imageData(),
-        data = imgdata.data;
+    var name = $(this).attr('id'),
+        value = $(this).val();
 
-    for(var i=0; i<data.length; i += 4){
-        data[i] = clamp(data[i]*value);
-        data[i+1] = clamp(data[i+1]*value);
-        data[i+2] = clamp(data[i+2]*value);
-    }
-    putData(imgdata);
-    lastfiltername = currentname;
-    current = imageData();
-    //$('#currentfilter').text('current: '+currentname);
-    //$('#lastfilter').text('last: '+lastfiltername);
-    //$('#beforestate').text(beforestate());
-
-    
-
+    update(name, value);    
 });
 
 
 $('#contrast').change(function(){
-           
-    var currentname = $(this).attr('id');
-    if(currentname != lastfiltername){
-        before = current;
-    }
-    revert();
-    var value = $(this).val(),
-        imgdata = imageData(),
-        data = imgdata.data;
-    
-    for(var i=0; i<data.length; i += 4){
-        var avg = Math.round((data[i]+data[i+1]+data[i+2]) / 3),
-            red = (avg >= 128) ? data[i]/value : data[i]*value,
-            green = (avg >= 128) ? data[i+1]/value : data[i+1]*value,
-            blue = (avg >= 128) ? data[i+2]/value : data[i+2]*value;
-              
-        data[i] = clamp(red);
-        data[i+1] = clamp(green);
-        data[i+2] = clamp(blue);
-    }
-    putData(imgdata);
-    lastfiltername = currentname;
-    current = imageData();
-    //$('#currentfilter').text('current: '+currentname);
-    //$('#lastfilter').text('last: '+lastfiltername);
-    //$('#beforestate').text(beforestate());
-   
+    var name = $(this).attr('id'),
+        value = $(this).val();
+
+    update(name, value);   
 });
 
 
 $('#grayscale').click(function(){
-    //revert();
-    var imgdata = imageData(),
-        data = imgdata.data;
-    
-    for(var i=0; i<data.length; i += 4){
-        var avg = Math.round((data[i]+data[i+1]+data[i+2]) / 3);
-        data[i] = data[i+1] = data[i+2] = avg;
-    }
-    putData(imgdata); 
+    var name = $(this).attr('id');
+
+    update(name, null);   
 });
 
 
 $('#invert').click(function(){
-    //revert();
-    var imgdata = imageData(),
-        data = imgdata.data;   
-    for(var i=0; i<data.length; i += 4){
-        data[i] = 255 - data[i];
-        data[i+1] = 255 - data[i+1];
-        data[i+2] = 255 - data[i+2];
-    }
-    putData(imgdata);
+    var name = $(this).attr('id');
+
+    update(name, null);  
  });
 
 
 $('#sepia').click(function(){
-    //revert();
-    var imgdata = imageData(),
-        data = imgdata.data;   
-    
-    for(var i=0; i<data.length; i += 4){
-        var red = Math.round((data[i] * .393)+(data[i+1] * .769)+(data[i+2]*.189)),
-            green = Math.round((data[i] * .349)+(data[i+1] * .686)+(data[i+2]*.168)),
-            blue = Math.round((data[i] * .272)+(data[i+1] * .534)+(data[i+2]*.131));
-        
-        data[i] = (red <= 255) ? red : 255;
-        data[i+1] = (green <= 255) ? green : 255;
-        data[i+2] = (blue <= 255) ? blue : 255;
-    }
-    putData(imgdata);
+    var name = $(this).attr('id');
+
+    update(name, null);
 });
 
 
